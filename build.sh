6e109f28b0c7c79dd160abd84a456a78bf72804a
Max <49086429+gucci-on-fleek@users.noreply.github.com>
2019-12-12 14:01:30 -0700

    Fix build script

--- build.sh
+++ build.sh
@@ -5,10 +5,11 @@
 apt update
 apt install ca-certificates git debhelper nodejs npm python python3-setuptools devscripts fakeroot git-lfs build-essential --no-install-recommends -y
 git clone --depth 1 https://github.com/mozilla-iot/gateway-deb.git
+cd gateway-deb
 git lfs pull
 tar xf webthings-gateway*
 cp -r debian gateway-*
 cd gateway-*
-npm i -g npm
+npm i -g npm # The default system npm tends to be really buggy
 ln -fs /usr/local/bin/npm /usr/bin/npm
 debuild -us -uc
