20e9e3ace5ed79a253f99f57d16818d92fbdf595
Max <49086429+gucci-on-fleek@users.noreply.github.com>
2019-12-12 20:18:24 -0700

    Add libpython3-dev as a Build-Depend

--- build.sh
+++ build.sh
@@ -3,13 +3,13 @@
 # This should be ran as root, preferably in a chroot or a container
 
 apt update
-apt install ca-certificates git debhelper nodejs npm python python3-setuptools devscripts fakeroot git-lfs build-essential --no-install-recommends -y
+apt install ca-certificates git debhelper nodejs npm python python3-setuptools devscripts fakeroot git-lfs build-essential python3-pip libnanomsg-dev libffi-dev libpython3-dev --no-install-recommends -y
 git clone --depth 1 https://github.com/gucci-on-fleek/gateway-deb.git
 cd gateway-deb
 git lfs pull
 tar xf webthings-gateway*
-cp -r debian gateway-*
-cd gateway-*
+cp -r debian webthings-gateway
+cd webthings-gateway
 npm i -g npm # The default system npm tends to be really buggy
 ln -fs /usr/local/bin/npm /usr/bin/npm
 debuild -us -uc
